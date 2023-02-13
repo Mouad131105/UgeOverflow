294d4bb37e4babaacde582beb3eed0f7eb67d3c4
Mouad131105 <mouad.aouida@gmail.com>
2023-02-13 16:03:54 +0100

    Ajout des routes (Tags, Questions, Users) dans Routes

--- app/src/main/java/fr/uge/ugeoverflow/routes/Routes.kt
+++ app/src/main/java/fr/uge/ugeoverflow/routes/Routes.kt
@@ -4,4 +4,7 @@ sealed class Routes(val route: String) {
     object Login : Routes("Login")
     object SignUp : Routes("SignUp")
     object ForgotPassword : Routes("ForgotPassword")
+    object Tags : Routes("Tags")
+    object Questions : Routes("Questions")
+    object Users : Routes("Users")
 }
--- app/src/main/java/fr/uge/ugeoverflow/ui/components/Maincomponent.kt
+++ app/src/main/java/fr/uge/ugeoverflow/ui/components/Maincomponent.kt
@@ -1,12 +1,17 @@
 package fr.uge.ugeoverflow.ui.components
-
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun mainComponent(){
-
 
 }
 
@@ -15,7 +20,17 @@ fun AppTopBar(
     onNavItemClick: () -> Unit,
     navController: NavHostController
 ){
-    TopAppBar() {
-        
-    }
+    TopAppBar(
+        title = {
+            Text(text = "Top App Bar")
+        },
+        navigationIcon = {
+            Button(onClick = { onNavItemClick() }, modifier = Modifier.width(IntrinsicSize.Min)) {
+            Icon(Icons.Default.Menu, "Home")
+        }},
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = White,
+        elevation = 10.dp
+
+    )
 }
