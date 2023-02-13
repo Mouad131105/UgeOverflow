4aa0732facf264aed7906011301e712ce6694567
Mouad131105 <mouad.aouida@gmail.com>
2023-02-13 15:50:13 +0100

    Ajout de fichier MainComponet pour le scafolding

---
+++ app/src/main/java/fr/uge/ugeoverflow/ui/components/Maincomponent.kt
@@ -0,0 +1,21 @@
+package fr.uge.ugeoverflow.ui.components
+
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun mainComponent(){
+
+
+}
+
+@Composable
+fun AppTopBar(
+    onNavItemClick: () -> Unit,
+    navController: NavHostController
+){
+    TopAppBar() {
+        
+    }
+}
