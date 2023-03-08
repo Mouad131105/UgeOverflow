0d84975a474e92c5443e7f0746d2bf3dc9efae2f
ACHRAF BETACH <ashrafbetach@gmail.com>
2023-03-08 16:21:46 +0100

    Adding Title and Description to TagDetails

--- app/src/main/java/fr/uge/ugeoverflow/screens/TagDetails.kt
+++ app/src/main/java/fr/uge/ugeoverflow/screens/TagDetails.kt
@@ -1,20 +1,27 @@
 package fr.uge.ugeoverflow.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import fr.uge.ugeoverflow.model.Question
 import fr.uge.ugeoverflow.model.Tag
 import fr.uge.ugeoverflow.ui.components.QuestionListItem
 
 @Composable
 fun TagDetails(tag: Tag, questions: List<Question>) {
-    Column {
-        Text(text = "Questions tagged [${tag.getTAG_TYPE}]", fontWeight = FontWeight.Bold)
-        Text(text = tag.getDescription.toString())
+    Column( Modifier.padding(15.dp)) {
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(text = "Questions tagged [${tag.getTAG_TYPE}]", fontSize = 24.sp)
+        }
+        Spacer(modifier = Modifier.fillMaxWidth().size(5.dp),)
+        Text(text = tag.getDescription.toString(), fontSize = 10.sp)
         LazyColumn {
             items(questions) { question ->
                 QuestionListItem(question = question)
